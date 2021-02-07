71fa32ad1bca911c56320e440e7677734729c69b
Albert Goncalves <goncalves.albert@gmail.com>
2021-02-07 16:38:03 -0500

    refine results plot

--- 01_ucb_admit/plot_results.py
+++ 01_ucb_admit/plot_results.py
@@ -5,6 +5,7 @@
 from os import environ
 from os.path import join
 
+from matplotlib.cm import get_cmap
 from matplotlib.pyplot import close, savefig, subplots, tight_layout
 from pandas import DataFrame, read_csv
 from seaborn import set_style
@@ -37,17 +38,34 @@ def main():
     set_style("darkgrid")
     (_, ax) = subplots(figsize=(6, 6))
     rows = data.male == 1
+    kwargs = {
+        "alpha": 0.725,
+    }
+    ax0 = ax.twinx()
+    ax0.bar(
+        data["index"],
+        data.applications,
+        color=data.dept.map(lambda i: get_cmap("Dark2")(i - 1)),
+        alpha=0.2,
+        zorder=1,
+    )
+    ax0.set_ylabel("applications")
+    ax0.grid(None)
     ax.scatter(
         data.loc[rows, "index"],
         data.loc[rows, "rate"],
         marker="s",
         label="obs male",
+        zorder=3,
+        **kwargs,
     )
     ax.scatter(
         data.loc[~rows, "index"],
         data.loc[~rows, "rate"],
         marker="s",
         label="obs female",
+        zorder=3,
+        **kwargs,
     )
     ax.scatter(
         preds["index"],
@@ -56,14 +74,21 @@ def main():
         edgecolor="dimgray",
         color="none",
         label="pred mean",
+        zorder=2,
+        **kwargs,
     )
     ax.vlines(
         preds["index"],
         (preds["mean"] - preds["std"]) / preds.applications,
         (preds["mean"] + preds["std"]) / preds.applications,
+        color="dimgray",
         label="pred 2*std",
+        zorder=1,
+        **kwargs,
     )
     ax.set_xticks(data["index"])
+    ax.set_ylim([0.0, 1.0])
+    ax.set_ylabel("admit rate")
     ax.legend()
     tight_layout()
     savefig(FILENAME["results"])
