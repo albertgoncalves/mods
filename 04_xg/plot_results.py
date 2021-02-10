212bb91ffe4d91840a4ddef46655fab31a219968
albertgoncalves <goncalves.albert@gmail.com>
2021-02-10 09:05:53 -0500

    clean-up plotting function

--- 04_xg/plot_results.py
+++ 04_xg/plot_results.py
@@ -3,6 +3,7 @@
 from os import environ
 from os.path import join
 
+from matplotlib.gridspec import GridSpec
 from matplotlib.pyplot import close, figure, savefig, tight_layout
 from numpy import empty
 from pandas import read_csv
@@ -18,55 +19,66 @@ FILENAME = {
 }
 
 
-def main():
+def load():
     data = read_csv(export_data.FILENAME["data"])
     export_data.set_data(data)
-    samples = read_csv(FILENAME["samples"])
-    mean = samples.mean()
+    return (data, read_csv(FILENAME["samples"]))
+
+
+def sim(samples):
     n = 201 * 91
-    xs = empty(n)
-    ys = empty(n)
+    sims = {
+        "x": empty(n),
+        "y": empty(n),
+    }
     i = 0
     for y in range(-45, 46, 1):
         for x in range(0, 201, 1):
-            xs[i] = x
-            ys[i] = y
+            sims["x"][i] = x
+            sims["y"][i] = y
             i += 1
-    shot_prob = expit(
-        norm.logpdf(xs, mean.shot_mu_x, mean.shot_sigma_x) +
-        norm.logpdf(ys, mean.shot_mu_y, mean.shot_sigma_y),
+    mean = samples.mean()
+    sims["shot_prob"] = expit(
+        norm.logpdf(sims["x"], mean.shot_mu_x, mean.shot_sigma_x) +
+        norm.logpdf(sims["y"], mean.shot_mu_y, mean.shot_sigma_y),
     )
-    shot_prob = \
-        (shot_prob - shot_prob.min()) / (shot_prob.max() - shot_prob.min())
-    goal_prob = bernoulli.pmf(1, expit(
-        norm.logpdf(xs, mean.goal_mu_x, mean.goal_sigma_x) +
-        norm.logpdf(ys, mean.goal_mu_y, mean.goal_sigma_y) +
+    sims["shot_prob"] = \
+        (sims["shot_prob"] - sims["shot_prob"].min()) / \
+        (sims["shot_prob"].max() - sims["shot_prob"].min())
+    sims["goal_prob"] = bernoulli.pmf(1, expit(
+        norm.logpdf(sims["x"], mean.goal_mu_x, mean.goal_sigma_x) +
+        norm.logpdf(sims["y"], mean.goal_mu_y, mean.goal_sigma_y) +
         mean.goal_offset,
     ))
+    return sims
+
+
+def plot(data, samples, sims):
     fig = figure(figsize=(10, 13.5), dpi=75)
-    gs = fig.add_gridspec(8, 2)
-    ax0 = fig.add_subplot(gs[:2, :])
-    ax1 = fig.add_subplot(gs[2:, 0])
-    ax2 = fig.add_subplot(gs[2:, 1])
+    gs = GridSpec(2, 2, figure=fig, height_ratios=[1, 5])
+    ax0 = fig.add_subplot(gs[0, :])
+    ax1 = fig.add_subplot(gs[1, 0])
+    ax2 = fig.add_subplot(gs[1, 1])
     histplot(
         samples.pred_goals,
         kde=True,
         discrete=True,
-        color="gray",
+        color="darkgray",
         ec="w",
         ax=ax0,
     )
     ax0.set_ylabel("sim goals")
-    ax0.axvline(data.goal.sum(), ls="--", color="tomato", label="obs goals")
+    ax0.axvline(data.goal.sum(), ls="--", color="r", lw=2, label="obs goals")
     ax0.legend()
     kwargs = {
         "cmap": "bone",
     }
     ax1.set_title("shot probability")
-    ax1.tricontourf(ys, xs, shot_prob, **kwargs)
+    ax1.tricontourf(sims["y"], sims["x"], sims["shot_prob"], **kwargs)
     scatterplot(x=data.y, y=data.x, ax=ax1)
     ax2.set_title("goal probability")
-    ax2.tricontourf(ys, xs, goal_prob, **kwargs)
+    ax2.tricontourf(sims["y"], sims["x"], sims["goal_prob"], **kwargs)
+    ax2.set_yticks([])
     scatterplot(
         x=data.y,
         y=data.x,
@@ -82,6 +94,11 @@ def main():
     tight_layout()
     savefig(FILENAME["results"])
     close()
+
+
+def main():
+    (data, samples) = load()
+    plot(data, samples, sim(samples))
 
 
 if __name__ == "__main__":
