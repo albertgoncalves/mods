48c78ab87f0810a5d923ea49af87df17b8fbf74f
albertgoncalves <goncalves.albert@gmail.com>
2024-12-02 21:45:14 -0500

    adjust nix dev env

--- shell.nix
+++ shell.nix
@@ -2,6 +2,7 @@ with import <nixpkgs> {};
 mkShell {
     buildInputs = [
         (python3.withPackages (ps: with ps; [
+            black
             flake8
             matplotlib
             pandas
@@ -24,4 +25,5 @@ mkShell {
     shellHook = ''
         . .shellhook
     '';
+    hardeningDisable = [ "all" ];
 }
